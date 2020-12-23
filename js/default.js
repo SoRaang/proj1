f4d52a831d9fe07fd2cae81383f516f7b6d05a3c
SoRaang <dsc0320@hotmail.com>
2020-12-23 12:36:18 +0900

    20121223_1236

--- js/default.js
+++ js/default.js
@@ -1,3 +1,17 @@
 $(document).ready(() => { // jQuery Load
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 500) {
+            $('#mnuQuick').fadeIn();
+        } else {
+            $('#mnuQuick').fadeOut(50);
+        }
+    });
 
+    $('.btnBurger').click(function() {
+        $('#mnuSitemap').animate({'right': '0'});
+    });
+
+    $('.sitemap_close').click(function() {
+        $(this).parent('aside').animate({'right': '-100%'});
+    });
 }) // jQuery Closed
