af3e6c001a08fa3a93046da308d75849545ea3a9
SoRaang <74249173+SoRaang@users.noreply.github.com>
2020-12-23 09:55:09 +0900

    Add files via upload

---
+++ js/default.js
@@ -0,0 +1,3 @@
+$(document).ready(() => { // jQuery Load
+
+}) // jQuery Closed
---
+++ js/jq_index.js
@@ -0,0 +1,38 @@
+$(document).ready(() => { // jQuery Load
+    $('span.close').click(function() {
+        $('#topBanner').slideUp();
+    })
+
+    $('.lazy').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        fade: true,
+        arrows: true,
+        dots: true,
+        zIndex: 1
+    });
+
+    $('.slideCompare').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: $('.comp_prev'),
+        nextArrow: $('.comp_next'),
+        dots: false,
+        zIndex: 1
+    });
+
+    let i;
+    i = setInterval(function() {
+        $('#noticeIndex > ul > li:first-child').animate({'margin-top': '-1rem'}, function(){
+            $(this).appendTo('#noticeIndex > ul');
+            $(this).css({'margin-top': '0'});
+        });
+    }, 5000);
+}) // jQuery Closed
